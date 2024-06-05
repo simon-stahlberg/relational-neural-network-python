f3231bd6d3239e8506b748e642255f9ae49a7491
Simon <simon.stahlberg@gmail.com>
2024-06-05 14:03:10 +0200

    Add simple train loop

---
+++ relnn_max.py
@@ -0,0 +1,108 @@
+import math
+import torch
+import torch.nn as nn
+
+from typing import List, Dict, Tuple
+from torch.nn.functional import mish
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size: int, output_size: int):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self._inner = nn.Linear(input_size, input_size, True)
+        self._outer = nn.Linear(input_size, output_size, True)
+        torch.nn.init.orthogonal_(self._inner.weight, math.sqrt(2))
+        torch.nn.init.constant_(self._inner.bias, 0.0)
+
+    def forward(self, input):
+        return self._outer(mish(self._inner(input)))
+
+
+class RelationMessagePassing(nn.Module):
+    def __init__(self, relation_name_arities: List[Tuple[str, int]], embedding_size: int):
+        super().__init__()
+        self._embedding_size = embedding_size
+        self._relation_mlps = nn.ModuleDict()
+        for relation_name, relation_arity in relation_name_arities:
+            input_size = relation_arity * embedding_size
+            output_size = relation_arity * embedding_size
+            if (input_size > 0) and (output_size > 0):
+                self._relation_mlps[relation_name] = MLP(input_size, output_size)
+        self._update_mlp = MLP(2 * embedding_size, embedding_size)
+
+    def forward(self, object_embeddings: torch.Tensor, relations: Dict[str, torch.Tensor]) -> torch.Tensor:
+        output_messages_list = []
+        output_indices_list = []
+        for relation_name, relation_module in self._relation_mlps.items():
+            if relation_name in relations:
+                relation_values = relations[relation_name]
+                input_embeddings = torch.index_select(object_embeddings, 0, relation_values).view(-1, relation_module.input_size)
+                output_messages = relation_module(input_embeddings).view(-1, self._embedding_size)
+                output_messages_list.append(output_messages)
+                output_indices_list.append(relation_values)
+        output_messages = torch.cat(output_messages_list, 0)
+        output_indices = torch.cat(output_indices_list, 0)
+        exps_max = torch.zeros_like(object_embeddings)
+        exps_max.index_reduce_(0, output_indices, output_messages, reduce="amax", include_self=False)
+        exps_max = exps_max.detach()
+        MAXIMUM_SMOOTHNESS = 12.0  # As the value approaches infinity, the hard maximum is attained
+        max_offsets = exps_max.index_select(0, output_indices).detach()
+        exps = (MAXIMUM_SMOOTHNESS * (output_messages - max_offsets)).exp()
+        exps_sum = torch.full_like(object_embeddings, 1E-16)
+        exps_sum.index_add_(0, output_indices, exps)
+        max_msg = ((1.0 / MAXIMUM_SMOOTHNESS) * exps_sum.log()) + exps_max
+        next_object_embeddings = object_embeddings + self._update_mlp(torch.cat([max_msg, object_embeddings], 1))
+        return next_object_embeddings
+
+
+class SumReadout(nn.Module):
+    def __init__(self, input_size: int, output_size: int):
+        super().__init__()
+        self._value = MLP(input_size, output_size)
+
+    def forward(self, object_embeddings: torch.Tensor, batch_sizes: List[int]) -> torch.Tensor:
+        cumsum_indices = torch.tensor(batch_sizes, device=object_embeddings.device).cumsum(0) - 1
+        cumsum_states = object_embeddings.cumsum(0).index_select(0, cumsum_indices)
+        aggregated_embeddings = torch.cat((cumsum_states[0].view(1, -1), cumsum_states[1:] - cumsum_states[0:-1]))
+        return self._value(aggregated_embeddings)
+
+
+class RelationalMessagePassingModule(nn.Module):
+    def __init__(self, relation_name_arities: List[Tuple[str, int]], embedding_size: int, num_layers: int):
+        super().__init__()
+        self._embedding_size = embedding_size
+        self._num_layers = num_layers
+        self._relation_network = RelationMessagePassing(relation_name_arities, embedding_size)
+        self._dummy = nn.Parameter(torch.empty(0))
+
+    def get_device(self):
+        return self._dummy.device
+
+    def forward(self, relations: Dict[str, torch.Tensor], batch_sizes: List[int]) -> torch.Tensor:
+        object_embeddings = self._initialize_nodes(sum(batch_sizes))
+        object_embeddings = self._pass_messages(object_embeddings, relations)
+        return object_embeddings
+
+    def _pass_messages(self, object_embeddings: torch.Tensor, relations: Dict[str, torch.Tensor]) -> torch.Tensor:
+        for _ in range(self._num_layers):
+            object_embeddings = self._relation_network(object_embeddings, relations)
+        return object_embeddings
+
+    def _initialize_nodes(self, num_objects: int) -> torch.Tensor:
+        object_embeddings = torch.zeros((num_objects, self._embedding_size), dtype=torch.float, device=self.get_device())
+        return object_embeddings
+
+
+class SmoothmaxRelationalNeuralNetwork(nn.Module):
+    def __init__(self, predicates: List[Tuple[str, int]], embedding_size: int, num_layers: int):
+        super().__init__()
+        predicates.sort()  # Ensure that relations are always processed in the same order
+        self._predicates = predicates
+        self._module = RelationalMessagePassingModule(predicates, embedding_size, num_layers)
+        self._readout = SumReadout(embedding_size, 1)
+
+    def forward(self, relations: Dict[str, torch.Tensor], batch_sizes: List[int]) -> torch.Tensor:
+        object_embeddings = self._module(relations, batch_sizes)
+        return self._readout(object_embeddings, batch_sizes)
--- train.py
+++ train.py
@@ -1,25 +1,29 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
 import argparse
-import pymimir
+import pymimir as mm
 import random
 
+from collections import defaultdict
 from pathlib import Path
-from typing import List, Tuple
+from relnn_max import SmoothmaxRelationalNeuralNetwork
+from typing import Dict, List, Tuple, Union
 
 
 class StateSpaceDataset:
-    def __init__(self, state_spaces: List[pymimir.StateSpace], num_items: int) -> None:
+    def __init__(self, state_spaces: List[mm.StateSpace], num_items: int) -> None:
         self._state_spaces = state_spaces
         self._num_items = num_items
 
-    def __len__(self):
+    def __len__(self) -> int:
         # We assume that the size of the dataset is 'self._num_items'.
         # This ensures predictable epoch lengths, regardless of the size of the given state spaces.
         # In the getitem method, we randomly sample states instead of systematically enumerating them.
         return self._num_items
 
-    def __getitem__(self, ignored_index) -> Tuple[pymimir.State, pymimir.StateSpace, int]:
+    def __getitem__(self, ignored_index) -> Tuple[mm.State, mm.StateSpace, int]:
         # We ignore the index and return a random state.
         # To achieve an even distribution, we uniformly sample a state space and select a valid goal-distance within that space.
         # Finally, we randomly sample a state from the selected state space and with the goal-distance.
@@ -40,11 +44,12 @@ def _parse_arguments() -> argparse.Namespace:
     parser.add_argument('--layers', default=30, type=int, help='Number of layers in the model')
     parser.add_argument('--batch_size', default=32, type=int, help='Number of samples per batch')
     parser.add_argument('--learning_rate', default=0.0002, type=float, help='Learning rate for the training process')
+    parser.add_argument('--num_epochs', default=1_000, type=int, help='Number of epochs for the training process')
     args = parser.parse_args()
     return args
 
 
-def _parse_instances(input: Path):
+def _parse_instances(input: Path) -> Tuple[str, List[str]]:
     print('Parsing files...')
     if input.is_file():
         domain_file = str(input.parent / 'domain.pddl')
@@ -56,12 +61,12 @@ def _parse_instances(input: Path):
     return domain_file, problem_files
 
 
-def _generate_state_spaces(domain_path: str, problem_paths: List[str]):
+def _generate_state_spaces(domain_path: str, problem_paths: List[str]) -> List[mm.StateSpace]:
     print('Generating state spaces...')
-    state_spaces: List[pymimir.StateSpace] = []
+    state_spaces: List[mm.StateSpace] = []
     for problem_path in problem_paths:
         print(f'> Expanding: {problem_path}')
-        state_space = pymimir.StateSpace.create(domain_path, problem_path, 1_000_000, 60_000)
+        state_space = mm.StateSpace.create(domain_path, problem_path, 1_000_000, 60_000)
         if state_space is not None:
             state_spaces.append(state_space)
             print(f'- # States: {state_space.get_num_states()}')
@@ -71,7 +76,7 @@ def _generate_state_spaces(domain_path: str, problem_paths: List[str]):
     return state_spaces
 
 
-def _create_datasets(state_spaces: List[pymimir.StateSpace]):
+def _create_datasets(state_spaces: List[mm.StateSpace]) -> Tuple[StateSpaceDataset, StateSpaceDataset]:
     print('Create datasets...')
     train_size = int(len(state_spaces) * 0.8)
     train_state_spaces = state_spaces[:train_size]
@@ -81,15 +86,102 @@ def _create_datasets(state_spaces: List[pymimir.StateSpace]):
     return train_dataset, validation_dataset
 
 
-def _train(train_dataset: pymimir.StateSpace, validation_dataset: pymimir.StateSpace):
+def _create_model(domain: mm.Domain, embedding_size: int, num_layers: int) -> nn.Module:
+    predicates = domain.get_static_predicates() + domain.get_fluent_predicates() + domain.get_derived_predicates()
+    relation_name_arities = [(predicate.get_name(), len(predicate.get_parameters())) for predicate in predicates]
+    model = SmoothmaxRelationalNeuralNetwork(relation_name_arities, embedding_size, num_layers)
+    return model
+
+
+def _get_atoms(state: mm.State, state_space: mm.StateSpace) -> List[Union[mm.StaticGroundAtom, mm.FluentGroundAtom, mm.DerivedGroundAtom]]:
+    problem = state_space.get_problem()
+    factories = state_space.get_factories()
+    static_atoms = [literal.get_atom() for literal in problem.get_static_initial_literals()]
+    fluent_atoms = factories.get_fluent_ground_atoms_from_ids(state.get_fluent_atoms())
+    derived_atoms = factories.get_derived_ground_atoms_from_ids(state.get_derived_atoms())
+    all_atoms = static_atoms + fluent_atoms + derived_atoms
+    return all_atoms
+
+
+def _get_goal(state_space: mm.StateSpace) -> List[Union[mm.StaticGroundAtom, mm.FluentGroundAtom, mm.DerivedGroundAtom]]:
+    problem = state_space.get_problem()
+    static_goal = [literal.get_atom() for literal in problem.get_static_goal_condition()]
+    fluent_goal = [literal.get_atom() for literal in problem.get_fluent_goal_condition()]
+    derived_goal = [literal.get_atom() for literal in problem.get_derived_goal_condition()]
+    full_goal = static_goal + fluent_goal + derived_goal
+    return full_goal
+
+
+def _group_term_ids_by_predicate_name(state_atoms: List[Union[mm.StaticGroundAtom, mm.FluentGroundAtom, mm.DerivedGroundAtom]],
+                                      goal_atoms: List[Union[mm.StaticGroundAtom, mm.FluentGroundAtom, mm.DerivedGroundAtom]]) -> Dict[str, List[int]]:
+    groups = defaultdict(list)
+    for atom in state_atoms:
+        predicate_name = atom.get_predicate().get_name()
+        term_ids = [term.get_identifier() for term in atom.get_objects()]
+        groups[predicate_name].extend(term_ids)
+    for atom in goal_atoms:
+        predicate_name = atom.get_predicate().get_name()
+        term_ids = [term.get_identifier() for term in atom.get_objects()]
+        groups[predicate_name + '_goal'].extend(term_ids)
+    return groups
+
+
+def _term_id_groups_to_tensors(term_id_groups: Dict[str, List[int]], device: torch.device) -> Dict[str, torch.Tensor]:
+    result = {}
+    for key, value in term_id_groups.items():
+        result[key] = torch.tensor(value, dtype=torch.int, device=device, requires_grad=False)
+    return result
+
+
+def _add_value_to_term_ids_in_group(group: Dict[str, List[int]], value: int) -> None:
+    for terms in group.values():
+        for index in range(len(terms)):
+            terms[index] += value
+
+
+def _create_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("GPU is available. Using GPU:", torch.cuda.get_device_name(0))
+    else:
+        device = torch.device("cpu")
+        print("GPU is not available. Using CPU.")
+    return device
+
+
+def _train(model: SmoothmaxRelationalNeuralNetwork, train_dataset: StateSpaceDataset, validation_dataset: StateSpaceDataset, num_epochs: int) -> None:
+    device = _create_device()
+    model = model.to(device)
+    state, state_space, goal_distance = train_dataset[0]
+    num_objects = len(state_space.get_problem().get_objects())
+    state_atoms = _get_atoms(state, state_space)
+    goal_atoms = _get_goal(state_space)
+    term_id_groups = _group_term_ids_by_predicate_name(state_atoms, goal_atoms)
+    input = _term_id_groups_to_tensors(term_id_groups, device)
+    # Training loop
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0002)
+    for epoch in range(0, num_epochs):
+        # Forward pass
+        output = model(input, [num_objects]).view(-1)
+        target = torch.tensor([goal_distance], dtype=torch.float, device=device)
+        loss: torch.Tensor = criterion(output, target)
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        # Print loss every 100 epochs
+        if (epoch + 1) % 100 == 0: print(f'[{epoch + 1}/{num_epochs}] Loss: {loss.item():.4f}')
     pass
 
 
-def _main(args: argparse.Namespace):
+def _main(args: argparse.Namespace) -> None:
     domain_path, problem_paths = _parse_instances(args.input)
     state_spaces = _generate_state_spaces(domain_path, problem_paths)
     train_dataset, validation_dataset = _create_datasets(state_spaces)
-    _train(train_dataset, validation_dataset)
+    domain = state_spaces[0].get_problem().get_domain()
+    model = _create_model(domain, args.embedding_size, args.layers)
+    _train(model, train_dataset, validation_dataset, args.num_epochs)
 
 
 if __name__ == "__main__":
